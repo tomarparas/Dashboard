a9314505eb129e3c46a1f328fa8d98b79baf51c7
tomarparas <130035621+tomarparas@users.noreply.github.com>
2024-08-17 10:36:14 +0530

    Dashboard

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,43 @@
+
+const Welcome = ({ children }: { children: React.ReactNode }) => {
+  return <div className="flex flex-col gap-y-4 py-7">{children}</div>;
+};
+
+const Title = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <h3 className="dark:text-whiteSecondary text-blackPrimary text-3xl font-bold">
+      {children}
+    </h3>
+  );
+};
+
+const Description = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <p className="text-xl dark:text-whiteSecondary text-blackPrimary">
+      {children}
+    </p>
+  );
+};
+
+const ActionButton = ({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick: () => void;
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className="dark:bg-whiteSecondary bg-blackPrimary dark:text-blackPrimary text-whiteSecondary w-60 py-3 font-semibold text-lg dark:hover:bg-white hover:bg-black duration-200"
+    >
+      {children}
+    </button>
+  );
+};
+
+Welcome.Title = Title;
+Welcome.Description = Description;
+Welcome.ActionButton = ActionButton;
+
+export default Welcome;
---
+++ src/components/WhiteButton.tsx
@@ -0,0 +1,32 @@
+
+
+import { Link } from "react-router-dom";
+
+const WhiteButton = ({
+  link,
+  text,
+  width,
+  py,
+  textSize,
+  children,
+}: {
+  link: string;
+  text: string;
+  width: string;
+  py: string;
+  textSize: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <Link
+      to={link}
+      className={`dark:bg-whiteSecondary bg-blackPrimary w-${width} py-${py} text-${textSize} dark:hover:bg-white hover:bg-gray-800 bg-blackPrimary duration-200 flex items-center justify-center gap-x-2`}
+    >
+      {children}
+      <span className="dark:text-blackPrimary text-whiteSecondary font-semibold">
+        {text}
+      </span>
+    </Link>
+  );
+};
+export default WhiteButton;
